bb10441965ad0292f71b69850a856e3759d7e457
jasmeet singh <89657743+zmontyyk@users.noreply.github.com>
2022-07-23 18:37:33 -1200

    Remove fun

--- src/components/Getting_Emp.tsx
+++ src/components/Getting_Emp.tsx
@@ -1,38 +1,36 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { Fetch_DATA, } from '../Actions/action'
-import { useDispatch, useSelector } from 'react-redux'
-import { employeeData } from '../models/models'
-import { Dispatch } from 'redux';
-import Remove_User from './Remove_User'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { Fetch_DATA } from "../Actions/action";
+import { useDispatch, useSelector } from "react-redux";
+import { employeeData } from "../models/models";
+import { Dispatch } from "redux";
+import Remove_User from "./Remove_User";
 
 function Getting_Emp() {
-  const dispatch = useDispatch<any>()
-  const [deleteID, setDeleteID] = useState<employeeData>()
-
-
-
-
-
-
+  const dispatch = useDispatch<any>();
+  const [deleteID, setDeleteID] = useState<any>();
+ 
+ 
   useEffect(() => {
-    dispatch(Fetch_DATA())
-  }, [])
+    dispatch(Fetch_DATA());
+  }, []);
 
   //  All employee Data  from store
   const All_Employee_Data = useSelector((state: any) => {
-    return state.FETCHING_DATA.userList
-  })
+    return state.FETCHING_DATA.userList;
+  });
 
-
-
-  const Edit=()=>{
-    alert('hey')
+  //  DELETE ID Clean up function
+  const cleanUp = () => {
+    setDeleteID({})
   }
 
+  const Edit = () => {
+    alert("hey");
+  }
 
   return (
-    <div className=''>
+    <div className="">
       <table className="table table-striped table-hover mt-5 ">
         <thead>
           <tr>
@@ -44,31 +42,39 @@ function Getting_Emp() {
             <th scope="col">Actions</th>
           </tr>
         </thead>
-        {All_Employee_Data.userList <0? <h1>hello</h1>:
-        <tbody>
-         
-            {All_Employee_Data.map((value:employeeData  ) => {
+        {All_Employee_Data.userList < 0 ? (
+          <h1>hello</h1>
+        ) : (
+          <tbody>
+            {All_Employee_Data.map((value: employeeData) => {
               return (
-                <React.Fragment key={value.id} >
-                   <tr>
-                  <th>{value.firstName}</th>
-                  <td>{value.lastName}</td>
-                  <td>{value.email}</td>
-                  <td>{value.age}</td>
-                  <td>{value.gender}</td>
-                  <td><i onClick={()=>setDeleteID(value)} className="fa-solid red fa-trash-can"></i>
-                  <i  onClick={()=>Edit()}  className="fa-solid EDIT fa-pen-to-square"></i>
-                  </td>
+                <React.Fragment key={value.id}>
+                  <tr>
+                    <th>{value.firstName}</th>
+                    <td>{value.lastName}</td>
+                    <td>{value.email}</td>
+                    <td>{value.age}</td>
+                    <td>{value.gender}</td>
+                    <td>
+                      <i
+                        onClick={() => setDeleteID(value)}
+                        className="fa-solid red fa-trash-can"
+                      ></i>
+                      <i
+                        onClick={() => Edit()}
+                        className="fa-solid EDIT fa-pen-to-square"
+                      ></i>
+                    </td>
                   </tr>
                 </React.Fragment>
-              )
+              );
             })}
-        </tbody>
-        }
+          </tbody>
+        )}
       </table>
-      <Remove_User deleteID={deleteID}  />
+      <Remove_User cleanUp={cleanUp} deleteID={deleteID} />
     </div>
-  )
+  );
 }
 
-export default Getting_Emp
+export default Getting_Emp;
--- src/components/Remove_User.tsx
+++ src/components/Remove_User.tsx
@@ -1,26 +1,39 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
-function Remove_User({ deleteID }: any) {
-  console.log(deleteID);
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+function Remove_User({ deleteID,cleanUp }: any) {
+ 
+  const [show, setShow] = useState(false) 
+ 
+  const handleClose =()=>{
+    setShow(false)
+    cleanUp()
+  }
+ 
+  
+
+
+  useMemo(()=>{
+   if (deleteID?.firstName?.length > 0) {
+    setShow(true)
+   }
+  },[deleteID])
+
+
+
 
   return (
     <div>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
 
-      <Modal show={show} onHide={handleClose}>
+
+      <Modal show={show} >
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Delete User !!!</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>You are about to delete <span style={{fontWeight: "bold"}} >{deleteID?.firstName}</span>  permanently</Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button  variant="secondary" onClick={handleClose}>
            NO !
           </Button>
           <Button variant="secondary" onClick={handleClose}>
